431fd68886f264da0c906a4a59995c7f51bb0c62
FoggyCaligo <136309172+FoggyCaligo@users.noreply.github.com>
2024-01-26 15:57:28 +0900

    unit_complete. 필요 페이지 파일 생성 & index.html로부터 다른 모든 페이지 전환 기능 구현.

--- index.js
+++ index.js
@@ -3,52 +3,39 @@
 function calc_change_btn(value){
     switch(value){
         case "퍼센트 계산기":
-            change2livpercent();
+            changeContent("./3_living/percent.html");
             break;
             
         case "요리 계량단위 변환기":
-            change2livunit();
+            changeContent("./3_living/unit.html");
             break;
 
         case "거리 단위 변환기":
+            changeContent("./3_living/dist.html");
             break;
             
         case "배당성장주 복리 계산기":
+            changeContent("./4_finance/devidend.html");
             break;
         
         case "주식 수익 계산기":
-            break;
+            changeContent("./4_finance/revenue.html");    
+        break;
 
         case "대소문자 변환":
-            change2textcapital();
+            // change2textcapital();
+            changeContent("./2_text/capital.html")
             break;
 
         case "글자수 & 발표시간 계산기":
             //alert(value)
-            change2textnspeech();
+            // change2textnspeech();
+            changeContent("./2_text/textNspeech/textNspeech.html");
             break;
     }   
 }
 
 
-
-
-
-//-----------------------------
-function change2textnspeech(){
-    document.getElementById("contentbox").innerHTML = '<object style="width:90vw; height:70vh;" type="text/html" data="./2_text/textNspeech/textNspeech.html"></object>';
+function changeContent(content_id){
+    document.getElementById("contentbox").innerHTML = '<object style="width:90vw; height:90vh;" type="text/html" data="'+content_id+'"></object>';
 }
-
-
-function change2textcapital(){
-    document.getElementById("contentbox").innerHTML = '<object style="width:90vw; height:70vh;" type="text/html" data="./2_text/capital.html"></object>';
-}
-//-----------------------------
-function change2livpercent(){
-    document.getElementById("contentbox").innerHTML = '<object style="width:90vw; height:70vh;" type="text/html" data="./3_living/percent.html"></object>';
-}
-
-function change2livunit(){
-    document.getElementById("contentbox").innerHTML = '<object style="width:90vw; height:70vh;" type="text/html" data="./3_living/unit.html"></object>';
-}
-
